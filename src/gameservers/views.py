b1b712ebee32ff8d6d71d5cbd5aa02709db7c694
A. Svensson <lmasvensson@gmail.com>
2015-03-09 16:28:10 +0100

    Updated the views and templates to reflect the new server model.

--- src/gameservers/views.py
+++ src/gameservers/views.py
@@ -13,23 +13,6 @@ class ServerDetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super(ServerDetailView, self).get_context_data(**kwargs)
         server = context['server']
-        context['weekly_history'] = server.get_history_stats(days=7)
-
-        stats = server.calc_player_stats(days=1)
-        context['daily_average'] = stats['players__avg']
-        context['daily_min'] = stats['players__min']
-        context['daily_max'] = stats['players__max']
-
-        stats = server.calc_player_stats(days=7)
-        context['weekly_average'] = stats['players__avg']
-        context['weekly_min'] = stats['players__min']
-        context['weekly_max'] = stats['players__max']
-
-        stats = server.calc_player_stats(days=31)
-        context['monthly_average'] = stats['players__avg']
-        context['monthly_min'] = stats['players__min']
-        context['monthly_max'] = stats['players__max']
-
-        context['weekday_averages'] = server.weekday_averages()
+        context['weekly_history'] = server.get_stats_history(days=7)
         return context
 
