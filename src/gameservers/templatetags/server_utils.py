d2c7c9bf2e3ac96fe3696f05b421c4de62eb5f1d
A. Svensson <lmasvensson@gmail.com>
2015-03-10 20:15:46 +0100

    Comment.

--- src/gameservers/templatetags/server_utils.py
+++ src/gameservers/templatetags/server_utils.py
@@ -8,5 +8,7 @@ register = template.Library()
 @register.filter
 def hours_since_now(timestamp):
     delta = timezone.now() - timestamp
+    # Since a datetime.timedelta only stores the delta in days/seconds/ms
+    # we have to convert the seconds to hours.
     return delta.total_seconds() / 3600.0
 
