6cf9d8d41cf6eecf8afb03a12e1ac44460990aca
A. Svensson <lmasvensson@gmail.com>
2015-03-15 19:08:18 +0100

    Redirected root index, a bunch of people have been hitting a 404 there.

--- src/src/urls.py
+++ src/src/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
+    # HACK: redirect root index. Might have to do something about it soon..
+    url(r'^$', RedirectView.as_view(url='/servers', permanent=False), name='index'),
     url(r'^servers/', include('gameservers.urls', namespace='gameservers')),
 )
