c07877dd66e0fe845707d65097e2af84c2cb4d21
A. Svensson <lmasvensson@gmail.com>
2015-05-19 18:17:45 +0200

    Updated plotter.

--- bin/plotter.sh
+++ bin/plotter.sh
@@ -11,13 +11,15 @@ gnuplot << EOF
 set datafile separator ","
 set xdata time
 set timefmt "%s" #time format of input data
+set format x "%d/%m" # format of output time
 
+set style data lines
+set style line 1 linewidth 2
+set grid
+unset key
+set title "$3" # If a second arg was supplied we show it as a title too
 set terminal png size 800,200 transparent truecolor
 set output "$2.png"
-set format x "%d/%m" # format of output time
-set grid
-set title "$3" # If a second arg was supplied we show it as a title too
-unset key
 
-plot "$1" every 4 using 1:2 with lines linewidth 2
+plot "$1" every 4 using 1:2 ls 1
 EOF
