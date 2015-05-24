8e04316a53aa2839c5f39cd7bf314a84bb55f294
A. Svensson <lmasvensson@gmail.com>
2015-05-24 14:53:14 +0200

    Added note for the future.

--- src/about/views.py
+++ src/about/views.py
@@ -6,6 +6,7 @@ from .utils import load_readme
 
 class AboutView(generic.TemplateView):
     template_name = 'about/about.html'
+    # The readme will only be loaded once at startup and then stored in mem.
     readme_md = load_readme()
 
     def get_context_data(self, **kwargs):
