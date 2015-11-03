2dc2aee78220dcea390845c170514759cfef0ca3
A. Svensson <lmasvensson@gmail.com>
2015-11-03 19:31:22 +0100

    Removed the old python code,
    and saved a copy of it in the old_python_code_dev branch.

--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-
-bind = "127.0.0.1:8082"
-workers = 2
-preload = True
-daemon = False
-log_level = 'info'
--- src/about/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls import patterns, url
-
-from .views import AboutView, StatsView
-
-urlpatterns = patterns('',
-    url(r'^$', AboutView.as_view(), name='index'),
-    url(r'^stats/$', StatsView.as_view(), name='stats'),
-)
